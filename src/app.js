16f7e8880c92f5cfee09bf5569532462effdd4eb
Aleksa-git <web.smart911@gmail.com>
2022-09-30 01:35:09 +0200

    showing current temperature, humidity, wind

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "9b4dd9de229ea5afe63c1fc8670585e8";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
